9a57b63457205fef097272e65aaaffb527aeb48c
Andrew Serputko <aserputko@gmail.com>
2017-04-24 19:14:07 +0300

    Add new code

---
+++ app/core/checkmark/checkmark-2.filter.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.
+  module('core').
+  filter('checkmark', function() {
+    return function(input) {
+      return input ? '\u2713' : '\u2718';
+    };
+  }).
+  filter('checkmark2', function() {
+    return function(input) {
+      return input ? '\u2713' : '\u2718';
+    };
+  }).
+  filter('checkmark3', function() {
+    return function(input) {
+      return input ? '\u2713' : '\u2718';
+    };
+  });
---
+++ app/core/phone/phone-2.service.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.
+  module('core.phone').
+  factory('Phone2', ['$resource',
+    function($resource) {
+      return $resource('phones/:phoneId.json', {}, {
+        query: {
+          method: 'GET',
+          params: {phoneId: 'phones'},
+          isArray: true
+        }
+      });
+    }
+  ]);
