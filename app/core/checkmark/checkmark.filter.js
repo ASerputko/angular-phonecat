2e4b4b7b64bb83dc88d66294b2176f10ff63a379
Andrew Serputko <aserputko@gmail.com>
2017-04-24 17:55:47 +0300

    Add new filter

--- app/core/checkmark/checkmark.filter.js
+++ app/core/checkmark/checkmark.filter.js
@@ -6,4 +6,9 @@ angular.
     return function(input) {
       return input ? '\u2713' : '\u2718';
     };
+  }).
+  filter('checkmark2', function() {
+    return function(input) {
+      return input ? '\u2713' : '\u2718';
+    };
   });
