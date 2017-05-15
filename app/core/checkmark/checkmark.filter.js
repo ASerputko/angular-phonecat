da46f367ddbb34d93f7d737453180366a653129d
Andrew Serputko <aserputko@gmail.com>
2017-05-15 15:13:33 +0300

    Add new code without unit tests

--- app/core/checkmark/checkmark.filter.js
+++ app/core/checkmark/checkmark.filter.js
@@ -1,8 +1,23 @@
 'use strict';
 
 angular.
-  module('core').
-  filter('checkmark', function() {
+  module('core')
+  .filter('checkmark', function() {
+    return function(input) {
+      return input ? '\u2713' : '\u2718';
+    };
+  })
+  .filter('checkmark2', function() {
+    return function(input) {
+      return input ? '\u2713' : '\u2718';
+    };
+  })
+  .filter('checkmark3', function() {
+    return function(input) {
+      return input ? '\u2713' : '\u2718';
+    };
+  })
+  .filter('checkmark4', function() {
     return function(input) {
       return input ? '\u2713' : '\u2718';
     };
